d929d40fa1e72c1d1addbc64be1b47892dc1f9da
FaLLen_GuY <talenairitya@gmail.com>
2021-03-31 19:23:54 +0530

    New source added

--- Mini-Projects/Youtube Downloader/Youtube Downloader.py
+++ Mini-Projects/Youtube Downloader/Youtube Downloader.py
@@ -7,14 +7,37 @@ print("-"*70,"\n")
 
 # import web library ...
 import webbrowser
-
+from pytube import YouTube
 # url Formatting ...
-url = input ("Enter the URL of the video : ")
+#url = input ("Enter the URL of the video : ")
 
-download = url[:12] + "ss" + url[12:]
-print( url[:12] + "ss" + url[12:])
+def savenet(url):
+    download = url[:12] + "ss" + url[12:]
+    print( url[:12] + "ss" + url[12:])
 
-# open browser to download file ...
-webbrowser.open(download,autoraise = True)
+    # open browser to download file ...
+    webbrowser.open(download,autoraise = True)
 
 
+def pytube(url):
+
+    video = YouTube(url)
+    stream = video.streams.get_highest_resolution()
+    stream.download()
+
+def Main():
+
+    url = input ("Enter the Video URL >> ")
+    choice = int(input("1: Download with SaveFrom.net\n2: Download with PyTube\n>> "))
+
+    if choice == 1:
+        savenet(url)
+
+    elif choice ==2:
+        pytube(url)
+
+    else:
+        print("Invalid Choice Try Againw..........\n")
+        Main()
+
+Main()
